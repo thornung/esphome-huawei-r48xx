2c2b3efe2ee97c09114288364a1d97173e83472f
jn30 <63346824+jn30@users.noreply.github.com>
2025-10-20 18:50:04 +0200

    Update __init__.py

--- components/huawei_r48xx/number/__init__.py
+++ components/huawei_r48xx/number/__init__.py
@@ -19,10 +19,8 @@ from esphome.const import (
 
 from .. import HuaweiR48xxComponent, huawei_r48xx_ns, CONF_HUAWEI_R48xx_ID
 
-
 CONF_OUTPUT_VOLTAGE = "output_voltage"
 CONF_MAX_OUTPUT_CURRENT = "max_output_current"
-
 
 HuaweiR48xxNumber = huawei_r48xx_ns.class_(
     "HuaweiR48xxNumber", number.Number, cg.Component
@@ -32,50 +30,33 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(CONF_HUAWEI_R48xx_ID): cv.use_id(HuaweiR48xxComponent),
-            cv.Optional(CONF_OUTPUT_VOLTAGE): number.NUMBER_SCHEMA.extend(
-                {
-                    cv.GenerateID(): cv.declare_id(HuaweiR48xxNumber),
-                    cv.Optional(CONF_MIN_VALUE, default=42): cv.float_,
-                    cv.Optional(CONF_MAX_VALUE, default=56): cv.float_,
-                    cv.Optional(CONF_STEP, default=0.1): cv.float_,
-                    cv.Optional(CONF_ICON, default=ICON_FLASH): cv.icon,
-                    cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_VOLT
-                    ): cv.string_strict,
-                    cv.Optional(CONF_MODE, default="BOX"): cv.enum(
-                        number.NUMBER_MODES, upper=True
-                    ),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_NONE
-                    ): cv.entity_category,
-                }
+            cv.Optional(CONF_OUTPUT_VOLTAGE): number.number_schema(
+                class_=HuaweiR48xxNumber,
+                min_value=42,
+                max_value=56,
+                step=0.1,
+                icon=ICON_FLASH,
+                unit_of_measurement=UNIT_VOLT,
+                mode="BOX",
+                entity_category=ENTITY_CATEGORY_NONE,
             ),
-            cv.Optional(CONF_MAX_OUTPUT_CURRENT): number.NUMBER_SCHEMA.extend(
-                {
-                    cv.GenerateID(): cv.declare_id(HuaweiR48xxNumber),
-                    cv.Optional(CONF_MIN_VALUE, default=0): cv.float_,
-                    cv.Optional(CONF_MAX_VALUE, default=45): cv.float_,
-                    cv.Optional(CONF_STEP, default=0.1): cv.float_,
-                    cv.Optional(CONF_ICON, default=ICON_CURRENT_AC): cv.icon,
-                    cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_AMPERE
-                    ): cv.string_strict,
-                    cv.Optional(CONF_MODE, default="BOX"): cv.enum(
-                        number.NUMBER_MODES, upper=True
-                    ),
-                    cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_NONE
-                    ): cv.entity_category,
-                }
+            cv.Optional(CONF_MAX_OUTPUT_CURRENT): number.number_schema(
+                class_=HuaweiR48xxNumber,
+                min_value=0,
+                max_value=45,
+                step=0.1,
+                icon=ICON_CURRENT_AC,
+                unit_of_measurement=UNIT_AMPERE,
+                mode="BOX",
+                entity_category=ENTITY_CATEGORY_NONE,
             ),
         }
     ).extend(cv.COMPONENT_SCHEMA)
 )
 
-
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_HUAWEI_R48xx_ID])
-    if config[CONF_OUTPUT_VOLTAGE]:
+    if config.get(CONF_OUTPUT_VOLTAGE):
         conf = config[CONF_OUTPUT_VOLTAGE]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
@@ -88,7 +69,7 @@ async def to_code(config):
         )
         cg.add(getattr(hub, "set_output_voltage_number")(var))
         cg.add(var.set_parent(hub, 0x0))
-    if config[CONF_MAX_OUTPUT_CURRENT]:
+    if config.get(CONF_MAX_OUTPUT_CURRENT):
         conf = config[CONF_MAX_OUTPUT_CURRENT]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
